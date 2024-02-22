3be16a0e52d9fedc66929139846949c9e3a4b383
Luiz Gabriel Lisboa Santos <lzg.lisboasantos5569@gmail.com>
2024-02-22 15:01:50 -0300

    adicionar primeiro commit com os arquivos

---
+++ jogoteca.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/inicio')
+def ola():
+  lista = ['Minecraft', 'Skyrim', 'Zelda']
+  return render_template('lista.html', titulo = 'Jogos', jogos = lista)
+
+app.run()
